e78c243a18d375bf779d661cb79096c59bf572ec
FrancoFernandez <36193803+FrancoFernandez@users.noreply.github.com>
2018-02-10 14:19:35 -0300

    Add files via upload

---
+++ jsInstruccionIF (3).js
@@ -0,0 +1,21 @@
+function Mostrar()
+{
+//tomo la edad  
+
+	var edad;
+
+	edad=document.getElementById('edad').value;
+
+	if(edad>17)
+	{
+
+		alert("es mayor");
+	}
+
+	else
+	{
+
+		alert("es menor");
+	}
+
+}//FIN DE LA FUNCIÓN
